ebc630c90ace255ba777a04a89051a868bedb35b
chu4zu9 <uesugirennn@yahoo.co.jp>
2021-06-07 05:57:20 +0900

    add:メモ編集画面を追加した.

--- App.jsx
+++ App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 
 import MemoDetailScreen from "./src/screens/MemoDetailScreen";
+import MemoEditScreen from "./src/screens/MemoEditScreen";
 import MemoListScreen from "./src/screens/MemoListScreen";
 
 export default function App() {
-  return <MemoListScreen />;
+  return <MemoEditScreen />;
 }

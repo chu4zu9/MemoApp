1e20039a7c3f8b753672b3772813f3837354203a
chu4zu9 <uesugirennn@yahoo.co.jp>
2021-06-06 16:11:43 +0900

    add: メモ詳細画面のスタイリングを追加した.

--- src/screens/MemoDetailScreen.jsx
+++ src/screens/MemoDetailScreen.jsx
@@ -6,19 +6,52 @@ import CircleButton from "../components/CircleButton";
 
 export default function MemoDetailScreen() {
   return (
-    <View>
+    <View style={styles.container}>
       <AppBar />
 
-      <View>
-        <Text>買い物リスト</Text>
-        <Text>2020年12月24日 10:00</Text>
+      <View style={styles.memoHeader}>
+        <Text style={styles.memoTitle}>買い物リスト</Text>
+        <Text style={styles.memoDate}>2020年12月24日 10:00</Text>
       </View>
 
-      <ScrollView>
-        <Text>買い物リスト</Text>
+      <ScrollView style={styles.memoBody}>
+        <Text style={styles.memoText}>買い物リスト</Text>
       </ScrollView>
 
-      <CircleButton>+</CircleButton>
+      <CircleButton style={{ top: 160, bottom: "auto" }}>+</CircleButton>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  memoHeader: {
+    backgroundColor: "#467fd3",
+    height: 96,
+    justifyContent: "center",
+    paddingVertical: 24,
+    paddingHorizontal: 19,
+  },
+  memoTitle: {
+    color: "#fff",
+    fontSize: 20,
+    lineHeight: 32,
+    fontWeight: "bold",
+  },
+  memoDate: {
+    color: "#fff",
+    fontSize: 12,
+    lineHeight: 16,
+  },
+  memoBody: {
+    paddingVertical: 32,
+    paddingHorizontal: 27,
+  },
+  memoText: {
+    fontSize: 16,
+    lineHeight: 24,
+  },
+});

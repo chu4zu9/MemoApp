0cea98b8b34a8566ee818b826e0893325f919b5f
chu4zu9 <uesugirennn@yahoo.co.jp>
2021-06-06 16:37:30 +0900

    change:propsにnameを追加し、childrenを削除した.

--- src/components/CircleButton.jsx
+++ src/components/CircleButton.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Feather } from "@expo/vector-icons";
 import { string, shape } from "prop-types";
 
 export default function CircleButton(props) {
-  const { children, style } = props;
+  const { style, name } = props;
   return (
     <View style={[styles.circleButton, style]}>
-      <Text style={styles.circleButtonRadius}>{children}</Text>
+      <Feather name={name} size={24} color="white" />
     </View>
   );
 }
 
 CircleButton.propTypes = {
-  children: string.isRequired,
+  name: string.isRequired,
   style: shape(),
 };
 

30e84fba42fd9970f5128e396e5d54471fa3a1f2
chu4zu9 <uesugirennn@yahoo.co.jp>
2021-06-07 20:23:23 +0900

    add:ログイン画面のスタイリングを追加.

--- src/screens/LogInScreen.jsx
+++ src/screens/LogInScreen.jsx
@@ -6,16 +6,16 @@ export default function LogInScreen() {
   return (
     <View style={styles.container}>
       <AppBar />
-      <View>
-        <Text>Log In</Text>
-        <TextInput value="Email Address" />
-        <TextInput value="Password" />
-        <View>
-          <Text>Submit</Text>
+      <View style={styles.inner}>
+        <Text style={styles.title}>Log In</Text>
+        <TextInput style={styles.input} value="Email Address" />
+        <TextInput style={styles.input} value="Password" />
+        <View style={styles.buttonContainer}>
+          <Text style={styles.buttonLabel}>Submit</Text>
         </View>
-        <View>
-          <Text>Not registerd?</Text>
-          <Text>Sign up here!</Text>
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>Not registerd?</Text>
+          <Text style={styles.footerLink}>Sign up here!</Text>
         </View>
       </View>
     </View>
@@ -23,5 +23,54 @@ export default function LogInScreen() {
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    backgroundColor: "#f0f4f8",
+  },
+  inner: {
+    paddingVertical: 24,
+    paddingHorizontal: 27,
+  },
+  title: {
+    fontSize: 24,
+    lineHeight: 32,
+    fontWeight: "bold",
+    marginBottom: 24,
+  },
+  input: {
+    fontSize: 16,
+    height: 48,
+    borderColor: "#dddddd",
+    borderWidth: 1,
+    backgroundColor: "#fff",
+    paddingVertical: 0,
+    paddingHorizontal: 8,
+    marginBottom: 16,
+  },
+  buttonContainer: {
+    backgroundColor: "#467fd3",
+    borderRadius: 4,
+    alignSelf: "flex-start",
+    marginBottom: 24,
+  },
+  buttonLabel: {
+    fontSize: 16,
+    lineHeight: 32,
+    paddingVertical: 8,
+    paddingHorizontal: 32,
+    color: "#fff",
+  },
+  footer: {
+    flexDirection: "row",
+  },
+  footerText: {
+    fontSize: 14,
+    lineHeight: 24,
+    marginRight: 8,
+  },
+  footerLink: {
+    fontSize: 14,
+    lineHeight: 24,
+    color: "#467fd3",
+  },
 });

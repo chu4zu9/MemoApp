b2eacbe0d6a6c6365520137b9b91da4cb25d1abb
chu4zu9 <uesugirennn@yahoo.co.jp>
2021-06-11 19:38:39 +0900

    change:React Navigationを使用してアプリが起動する様に変更.

--- App.jsx
+++ App.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 import SignUpScreen from "./src/screens/SignUpScreen";
 import LogInScreen from "./src/screens/LogInScreen";
@@ -7,6 +9,14 @@ import MemoDetailScreen from "./src/screens/MemoDetailScreen";
 import MemoEditScreen from "./src/screens/MemoEditScreen";
 import MemoListScreen from "./src/screens/MemoListScreen";
 
+const Stack = createStackNavigator();
+
 export default function App() {
-  return <MemoListScreen />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={MemoListScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }

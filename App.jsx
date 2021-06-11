1e9b1def7a854a72c71dde814122293b8a29aa25
chu4zu9 <uesugirennn@yahoo.co.jp>
2021-06-11 20:07:28 +0900

    add:すべてのスクリーンをReact Navigationに登録.

--- App.jsx
+++ App.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-// import SignUpScreen from './src/screens/SignUpScreen';
-// import LogInScreen from './src/screens/LogInScreen';
-// import MemoCreateScreen from './src/screens/MemoCreateScreen';
-// import MemoDetailScreen from './src/screens/MemoDetailScreen';
-// import MemoEditScreen from './src/screens/MemoEditScreen';
+import SignUpScreen from './src/screens/SignUpScreen';
+import LogInScreen from './src/screens/LogInScreen';
+import MemoCreateScreen from './src/screens/MemoCreateScreen';
+import MemoDetailScreen from './src/screens/MemoDetailScreen';
+import MemoEditScreen from './src/screens/MemoEditScreen';
 import MemoListScreen from './src/screens/MemoListScreen';
 
 const Stack = createStackNavigator();
@@ -14,8 +14,31 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={MemoListScreen} />
+      <Stack.Navigator initialRouteName="LogIn">
+        <Stack.Screen
+          name="MemoList"
+          component={MemoListScreen}
+        />
+        <Stack.Screen
+          name="MemoDetail"
+          component={MemoDetailScreen}
+        />
+        <Stack.Screen
+          name="MemoEdit"
+          component={MemoEditScreen}
+        />
+        <Stack.Screen
+          name="MemoCreate"
+          component={MemoCreateScreen}
+        />
+        <Stack.Screen
+          name="LogIn"
+          component={LogInScreen}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUpScreen}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );

3d9ea2dac918c6d0b20c1f603465293103676de6
chu4zu9 <uesugirennn@yahoo.co.jp>
2021-06-17 06:24:34 +0900

    add:SignUp機能を追加.

--- src/screens/SignUpScreen.jsx
+++ src/screens/SignUpScreen.jsx
@@ -5,7 +5,9 @@ import {
   Text,
   TextInput,
   TouchableOpacity,
+  Alert,
 } from 'react-native';
+import firebase from 'firebase';
 
 import Button from '../components/Button';
 
@@ -13,6 +15,22 @@ export default function SignUpScreen(props) {
   const { navigation } = props;
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  function handlePress() {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        const { user } = userCredential;
+        console.log(user.uid);
+        navigation.reset({
+          index: 0,
+          routes: [{ name: 'MemoList' }],
+        });
+      })
+      .catch((error) => {
+        Alert.alert(error.code);
+      });
+  }
   return (
     <View style={styles.container}>
       <View style={styles.inner}>
@@ -39,15 +57,7 @@ export default function SignUpScreen(props) {
           secureTextEntry
           textContentType="password"
         />
-        <Button
-          label="Submit"
-          onPress={() => {
-            navigation.reset({
-              index: 0,
-              routes: [{ name: 'MemoList' }],
-            });
-          }}
-        />
+        <Button label="Submit" onPress={handlePress} />
         <View style={styles.footer}>
           <Text style={styles.footerText}>
             Already registerd?

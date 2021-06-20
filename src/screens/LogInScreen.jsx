e7e1ecb948cd05af6c7b7592db00624975242e35
chu4zu9 <uesugirennn@yahoo.co.jp>
2021-06-20 12:13:02 +0900

    add:ログイン中やメモリスト読み込み中にLoadingコンポーネントを表示する様にした.

--- src/screens/LogInScreen.jsx
+++ src/screens/LogInScreen.jsx
@@ -10,11 +10,13 @@ import {
 import firebase from 'firebase';
 
 import Button from '../components/Button';
+import Loading from '../components/Loading';
 
 export default function LogInScreen(props) {
   const { navigation } = props;
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
     const unsubscribe = firebase
@@ -25,12 +27,15 @@ export default function LogInScreen(props) {
             index: 0,
             routes: [{ name: 'MemoList' }],
           });
+        } else {
+          setLoading(false);
         }
       });
     return unsubscribe;
   }, []);
 
   function handlePress() {
+    setLoading(true);
     firebase
       .auth()
       .signInWithEmailAndPassword(email, password)
@@ -44,11 +49,15 @@ export default function LogInScreen(props) {
       })
       .catch((error) => {
         Alert.alert(error.code);
+      })
+      .then(() => {
+        setLoading(false);
       });
   }
 
   return (
     <View style={styles.container}>
+      <Loading isLoading={isLoading} />
       <View style={styles.inner}>
         <Text style={styles.title}>Log In</Text>
         <TextInput

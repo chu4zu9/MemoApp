9e2dc547412a978434e62be07df495d79433e03b
chu4zu9 <uesugirennn@yahoo.co.jp>
2021-06-20 10:29:35 +0900

    add:メモの詳細を画面に表示する処理を追加.

--- src/screens/MemoDetailScreen.jsx
+++ src/screens/MemoDetailScreen.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   StyleSheet,
   Text,
@@ -6,24 +6,53 @@ import {
   ScrollView,
 } from 'react-native';
 import { shape, string } from 'prop-types';
+import firebase from 'firebase';
 
 import CircleButton from '../components/CircleButton';
 
 export default function MemoDetailScreen(props) {
   const { navigation, route } = props;
   const { id } = route.params;
+  const [memo, setMemo] = useState(null);
+
   console.log(id);
+
+  useEffect(() => {
+    const { currentUser } = firebase.auth();
+    let unsubscribe = () => {};
+    if (currentUser) {
+      const db = firebase.firestore();
+      const ref = db
+        .collection(`users/${currentUser.uid}/memos`)
+        .doc(id);
+      unsubscribe = ref.onSnapshot((doc) => {
+        console.log(doc.id, doc.data());
+        const data = doc.data();
+        setMemo({
+          id: doc.id,
+          bodyText: data.bodyText,
+          updatedAt: data.updatedAt.toDate(),
+        });
+      });
+    }
+    return unsubscribe;
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.memoHeader}>
-        <Text style={styles.memoTitle}>買い物リスト</Text>
+        <Text style={styles.memoTitle} numberOfLines={1}>
+          {memo && memo.bodyText}
+        </Text>
         <Text style={styles.memoDate}>
-          2020年12月24日 10:00
+          {memo && String(memo.updatedAt)}
         </Text>
       </View>
 
       <ScrollView style={styles.memoBody}>
-        <Text style={styles.memoText}>買い物リスト</Text>
+        <Text style={styles.memoText}>
+          {memo && memo.bodyText}
+        </Text>
       </ScrollView>
 
       <CircleButton

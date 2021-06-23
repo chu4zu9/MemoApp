ffb6a7071a4a1e7266f50a1d06d092300cd6b0ac
chu4zu9 <uesugirennn@yahoo.co.jp>
2021-06-23 20:15:34 +0900

    fix:スクロールバーが左に寄っていた問題を修正.

--- src/screens/MemoEditScreen.jsx
+++ src/screens/MemoEditScreen.jsx
@@ -71,8 +71,6 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   inputContainer: {
-    paddingVertical: 32,
-    paddingHorizontal: 27,
     flex: 1,
   },
   input: {
@@ -80,5 +78,8 @@ const styles = StyleSheet.create({
     textAlignVertical: 'top',
     fontSize: 16,
     lineHeight: 24,
+    paddingTop: 32,
+    paddingBottom: 32,
+    paddingHorizontal: 27,
   },
 });
